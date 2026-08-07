184f251a760b0548270e58000536fe6b86e78f44
caiomunhoz <127498717+caiomunhoz@users.noreply.github.com>
2026-08-07 13:22:02 -0300

    feat: manage resources with remote state data source

---
+++ remote-state-data-source/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 1.15.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tf-remote-state-10d5ad762746cac680247ac8ed"
+    key     = "vm/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "tf"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "tf"
+  default_tags {
+    tags = {
+      "owner"      = "caiomunhoz"
+      "managed-by" = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "tf-remote-state-10d5ad762746cac680247ac8ed"
+    key    = "vpc/terraform.tfstate"
+    region = "us-east-1"
+    profile = "tf"
+  }
+}
